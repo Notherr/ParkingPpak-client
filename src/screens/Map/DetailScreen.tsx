644d4e95e25c2fb3d7cf09c157b05b57a46b2d87
sewonshin <68217675+shinsewon@users.noreply.github.com>
2022-03-17 00:17:13 +0900

    Revert "PR 테스트중입니다."

--- src/screens/Map/DetailScreen.tsx
+++ src/screens/Map/DetailScreen.tsx
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function DetailScreen() {
   return (
     <View style={styles.block}>
-      <Text>DetailScreen12</Text>
+      <Text>DetailScreen1</Text>
     </View>
   );
 }
