20e31f2dfd251f7e835b466196ce16ed7c7ffc99
sewon <shin6403@gmail.com>
2022-03-30 18:36:35 +0900

    naverMap 연동작업

--- src/screens/Map/MapScreen.tsx
+++ src/screens/Map/MapScreen.tsx
@@ -1,13 +1,63 @@
 import React from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {NaverMap} from 'components/Map';
-import {View} from 'react-native';
+import {View, Pressable, StyleSheet, StatusBar, Platform} from 'react-native';
 
-function MapScreen() {
+function MapScreen({navigation}: NativeStackScreenProps<any>) {
+  const {top} = useSafeAreaInsets();
+  const goToBackScreen = () => navigation.pop();
+
   return (
-    <View>
+    <View style={{flex: 1}}>
+      <StatusBar />
+      <View style={[styles.wrapper, {top}]}>
+        <Pressable
+          style={({pressed}) => [
+            styles.button,
+            Platform.OS === 'ios' && {opacity: pressed ? 0.6 : 1},
+          ]}
+          android_ripple={{color: '#fff'}}
+          onPress={goToBackScreen}>
+          <Icon name="arrow-back-ios" size={24} style={styles.icon} />
+        </Pressable>
+      </View>
       <NaverMap />
     </View>
   );
 }
 
 export default MapScreen;
+
+const styles = StyleSheet.create({
+  backButton: {
+    borderWidth: 2,
+    borderColor: 'red',
+  },
+  wrapper: {
+    position: 'absolute',
+    left: 50,
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    shadowColor: '#4d4d4d',
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
+    overflow: Platform.select({android: 'hidden'}),
+    zIndex: 3,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 28,
+    backgroundColor: '#009688',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  icon: {
+    color: '#fff',
+  },
+});
--- src/screens/Map/index.ts
+++ src/screens/Map/index.ts
@@ -1,3 +1,2 @@
 export {default as MapStack} from './MapStack';
-export {default as DetailScreen} from './DetailScreen';
 export {default as MapScreen} from './MapScreen';
