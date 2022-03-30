abba19a6cbf77790020226c33fba83c7543c13ed
sewon <shin6403@gmail.com>
2022-03-30 17:23:13 +0900

    [✨ Add] Naver Map 연동작업 -. AOS,IOS Naver Map 연동 작업

---
+++ src/screens/Map/MapScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NaverMap} from 'components/Map';
+import {View} from 'react-native';
+
+function MapScreen() {
+  return (
+    <View>
+      <NaverMap />
+    </View>
+  );
+}
+
+export default MapScreen;
---
+++ src/screens/Map/index.ts
@@ -0,0 +1,3 @@
+export {default as MapStack} from './MapStack';
+export {default as DetailScreen} from './DetailScreen';
+export {default as MapScreen} from './MapScreen';
