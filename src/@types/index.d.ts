503cb9cfa918a68fa50b61edb78b81999ee8ac4b
hyewon do <tsc03018@gmail.com>
2022-06-12 16:34:45 +0900

    feat: 찜목록 퍼블리싱

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -15,4 +15,37 @@ declare global {
     logo: T;
     title: T;
   };
+
+  // 데이터 타입 확인 필요
+  type ParkingLot = {
+    id: number;
+    add_rates: number;
+    add_time_rates: number;
+    address: string;
+    holiday_begin: string;
+    holiday_end: string;
+    modification_date: number;
+    parking_code: string;
+    parking_name: string;
+    payyn: boolean;
+    phone_number: number;
+    rates: number;
+    sync_time: string;
+    time_rates: string;
+    type: number;
+    weekday_begin: string;
+    weekday_end: string;
+    weekend_begin: string;
+    weekend_end: string;
+    x_coor: number;
+    y_coor: number;
+  };
+
+  // 추가 필요
+  type GasStation = {
+    id: number;
+    compName: string;
+    xCoor: number;
+    yCoor: number;
+  };
 }
---
+++ src/screens/MainTab/SettingStack/LikeScreen.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import {useWindowDimensions} from 'react-native';
+import {palette} from '@/constant';
+import ParkingLotCard from '@/components/Setting/ParkingLotCard';
+import GasStationCard from '@/components/Setting/GasStationCard';
+import {TabView, TabBar, SceneRendererProps} from 'react-native-tab-view';
+import {FlatList} from 'react-native-gesture-handler';
+
+type RouteType = {
+  key: ContentType;
+  title: string;
+};
+
+const dataList = [
+  {title: 'mock data1', like: true, onPress: () => undefined},
+  {title: 'mock data2', like: false, onPress: () => undefined},
+  {title: 'mock data3', like: true, onPress: () => undefined},
+  {title: 'mock data4', like: false, onPress: () => undefined},
+  {title: 'mock data5', like: true, onPress: () => undefined},
+  {title: 'mock data6', like: false, onPress: () => undefined},
+  {title: 'mock data7', like: true, onPress: () => undefined},
+  {title: 'mock data8', like: false, onPress: () => undefined},
+  {title: 'mock data9', like: true, onPress: () => undefined},
+  {title: 'mock data10', like: false, onPress: () => undefined},
+  {title: 'mock data11', like: true, onPress: () => undefined},
+  {title: 'mock data12', like: false, onPress: () => undefined},
+  {title: 'mock data13', like: true, onPress: () => undefined},
+];
+
+const ParkingRoute = () => (
+  <FlatList<{title: string; like: boolean}>
+    data={dataList}
+    renderItem={({item}) => (
+      <ParkingLotCard {...item} onPress={() => undefined} />
+    )}
+    keyExtractor={item => item.title}
+  />
+);
+
+const OilRoute = () => (
+  <FlatList<{title: string; like: boolean}>
+    data={dataList}
+    renderItem={({item}) => (
+      <GasStationCard {...item} onPress={() => undefined} />
+    )}
+    keyExtractor={item => item.title}
+  />
+);
+
+const renderScene = ({
+  route,
+}: SceneRendererProps & {
+  route: RouteType;
+}) => {
+  switch (route.key) {
+    case 'PARKING_LOT':
+      return <ParkingRoute />;
+    case 'GAS_STATION':
+      return <OilRoute />;
+    default:
+      return <></>;
+  }
+};
+
+const ROUTES: RouteType[] = [
+  {key: 'PARKING_LOT', title: '주차장'},
+  {key: 'GAS_STATION', title: '주유소'},
+];
+
+export default function TabViewExample() {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState<number>(0);
+
+  return (
+    <TabView
+      navigationState={{index, routes: ROUTES}}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{width: layout.width}}
+      renderTabBar={props => (
+        <TabBar
+          {...props}
+          indicatorStyle={{backgroundColor: palette.blue_1}}
+          style={{
+            backgroundColor: palette.white,
+            shadowOffset: {height: 0, width: 0},
+            shadowColor: 'transparent',
+          }}
+          labelStyle={{color: palette.blue_1, fontWeight: 'bold'}}
+          pressOpacity={0.7}
+        />
+      )}
+    />
+  );
+}
